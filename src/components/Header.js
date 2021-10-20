444994c17e71ff056fcc4631c063b311c51bb60f
Nayeem <nayeem9812@gmail.com>
2021-10-20 15:53:16 +0600

    date and time dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+
+
 
 export default function Header() {
+    const [dateState, setDateState] = useState(new Date());
+
+    useEffect(() => {
+        setInterval(() => setDateState(new Date()), 1000);
+    }, []);
+
     return (
         <>
         <header className="header box">
@@ -9,7 +17,20 @@ export default function Header() {
                 <img className="header__photo-img" src="assets/img/logo/logo.png" alt="DoTab" />
             </div>
             <div className="header__base-info text-center w-100">
-                <div className="status text-white dateTime">{new Date().toLocaleString() + ''}</div>
+                <div className="status text-white dateTime">
+                {dateState.toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                 })}
+                 { ' ' }
+                 {dateState.toLocaleString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true,
+                })}
+                </div>
                 <p>Something will appear here in later.........</p>
             </div>
             </div>
